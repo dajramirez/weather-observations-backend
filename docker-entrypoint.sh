c0c38f9bbad6faf00e4e26403d8b2708261edda6
Darío J. Ramírez <dariojesusramirez@gmail.com>
2026-03-30 20:33:53 +0200

    revert(docker): remove temporary DB connection debug check from entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,4 @@ php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 
-# Mostrar el error de Laravel en los logs
-php artisan tinker --execute="DB::connection()->getPdo();" 2>&1 || echo "DB CONNECTION FAILED"
-
 apache2-foreground
