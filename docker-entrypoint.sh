f41a11ea58a1516c8d5f3802e19954ab0ca33b04
Darío J. Ramírez <dariojesusramirez@gmail.com>
2026-03-30 20:40:06 +0200

    debug: configure Apache to use dynamic PORT env variable for Render deployment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,4 +9,8 @@ php artisan config:clear
 php artisan config:cache
 php artisan route:cache
 
+# Render asigna el puerto dinámicamente
+sed -i "s/Listen 80/Listen ${PORT:-80}/" /etc/apache2/ports.conf
+sed -i "s/:80>/:${PORT:-80}>/" /etc/apache2/sites-available/000-default.conf
+
 apache2-foreground
